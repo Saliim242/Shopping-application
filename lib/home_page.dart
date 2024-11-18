286dcc7a36a28c1453db4633279c26962348b08c
Salim Abukar Ahmed <116594165+Saliim242@users.noreply.github.com>
2024-11-18 16:34:07 +0300

    Added Expanded and the list filters

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,27 +3,59 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  final List<String> filters = const [
+    "All",
+    "Addids",
+    "Sneakers",
+    "Boots",
+    "Sandals",
+    "Heels",
+    "Flats",
+    "Nike",
+  ];
+
   @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color.fromRGBO(255, 255, 255, 1),
+      ),
+      borderRadius: BorderRadius.horizontal(
+        left: Radius.circular(50),
+      ),
+    );
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                "Shoes Collection",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Text(
+                    "Shoes\nCollection",
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
-              SizedBox(width: 20),
-              Expanded(
-                child: TextField(),
-              )
-            ],
-          )
-        ],
+                SizedBox(width: 20),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: border,
+                      enabledBorder: border,
+                      focusedBorder: border,
+                      hintText: 'Searche',
+                      prefixIcon: Icon(Icons.search),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Shopping App",
       theme: ThemeData(
         fontFamily: 'Lato',
         colorScheme: ColorScheme.fromSeed(
           seedColor: Color.fromRGBO(254, 206, 1, 1),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Color.fromRGBO(119, 119, 119, 1),
+          ),
+          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
         ),
       ),
       home: HomePage(),
