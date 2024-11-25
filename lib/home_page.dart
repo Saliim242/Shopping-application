0d12035075a5c7e3b52b93ee4e0373b9a05f3ab1
Salim Abukar Ahmed <116594165+Saliim242@users.noreply.github.com>
2024-11-25 05:52:19 +0300

    Added Product data and their images

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final List<String> filters = const [
     "All",
     "Addids",
@@ -12,13 +17,22 @@ class HomePage extends StatelessWidget {
     "Heels",
     "Flats",
     "Nike",
+    "Bets",
   ];
+
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    selectedFilter = filters[0];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     const border = OutlineInputBorder(
       borderSide: BorderSide(
-        color: Color.fromRGBO(255, 255, 255, 1),
+        color: Color.fromRGBO(189, 189, 189, 1),
       ),
       borderRadius: BorderRadius.horizontal(
         left: Radius.circular(50),
@@ -53,6 +67,43 @@ class HomePage extends StatelessWidget {
                   ),
                 )
               ],
+            ),
+            SizedBox(
+              height: 125,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: filters.length,
+                itemBuilder: (context, int index) {
+                  final String filter = filters[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        print(selectedFilter);
+                        selectedFilter = filter;
+                        setState(() {});
+                      },
+                      child: Chip(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        side: BorderSide(
+                          color: Color.fromRGBO(245, 247, 249, 1),
+                        ),
+                        backgroundColor: selectedFilter == filter
+                            ? Theme.of(context).colorScheme.primary
+                            : Color.fromRGBO(245, 247, 249, 1),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        label: Text(filter),
+                        labelStyle: TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             )
           ],
         ),
